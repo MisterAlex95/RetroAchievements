18d5a33b54910210fdb855e99ff21b6da02fea8d
Alex Dana <alex.dana2013@gmail.com>
2024-07-06 13:10:46 +0200

    add graphical lib

--- app/Login.tsx
+++ app/Login.tsx
@@ -1,33 +1,87 @@
+import {
+  AlertCircleIcon,
+  Box,
+  Button,
+  ButtonText,
+  FormControl,
+  FormControlError,
+  FormControlErrorIcon,
+  FormControlErrorText,
+  FormControlHelper,
+  FormControlHelperText,
+  FormControlLabel,
+  FormControlLabelText,
+  Input,
+  InputField,
+  Heading,
+  Link,
+  LinkText,
+} from "@gluestack-ui/themed";
+
 import { useState } from "react";
-import { Text, View, TextInput } from "react-native";
 
 export default function Login() {
   const [userName, setUsername] = useState("");
   const [apiKey, setApiKey] = useState("");
 
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Login page.</Text>
-      <View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <Text>Enter your username:</Text>
-          <TextInput
-            onChangeText={(text) => setUsername(text)}
-          />
-        </View>
-        <View style={{ flex: 1, flexDirection: "row" }}>
-          <Text>Enter your APIKey:</Text>
-          <TextInput
-            onChangeText={(text) => setApiKey(text)}
-          />
-        </View>
-      </View>
-    </View>
+    <Box>
+      <Heading>Login page.</Heading>
+      <Box mt="$10">
+        <Box h="$32" w="$72">
+          <FormControl
+            size="md"
+            isDisabled={false}
+            isInvalid={false}
+            isReadOnly={false}
+            isRequired={false}
+          >
+            <Box mb="$10">
+              <FormControlLabel mb="$1">
+                <FormControlLabelText>Username </FormControlLabelText>
+              </FormControlLabel>
+
+              <Input>
+                <InputField
+                  type="text"
+                  defaultValue=""
+                  placeholder="username"
+                />
+              </Input>
+            </Box>
+
+            <FormControlLabel mb="$1">
+              <FormControlLabelText>API Key</FormControlLabelText>
+            </FormControlLabel>
+
+            <Input>
+              <InputField type="text" defaultValue="" placeholder="apiKey" />
+            </Input>
+
+            <FormControlHelper>
+              <FormControlHelperText>
+                you can find it{" "}
+                <Link href="https://retroachievements.org/controlpanel.php">
+                  <LinkText marginVertical="$0" size="xs">
+                    here
+                  </LinkText>
+                </Link>
+              </FormControlHelperText>
+            </FormControlHelper>
+
+            <FormControlError>
+              <FormControlErrorIcon as={AlertCircleIcon} />
+              <FormControlErrorText>
+                At least 6 characters are required.
+              </FormControlErrorText>
+            </FormControlError>
+
+            <Button>
+              <ButtonText>Login</ButtonText>
+            </Button>
+          </FormControl>
+        </Box>
+      </Box>
+    </Box>
   );
 }
--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
-import { View } from "react-native";
+import { GluestackUIProvider, Box } from "@gluestack-ui/themed";
+import { config } from "@gluestack-ui/config"; // Optional if you want to use default theme
 import Login from "./Login";
 
-export default function Index() {
+export default function App() {
   return (
-    <View style={{ flex: 1 }}>
-      <Login />
-    </View>
+    <GluestackUIProvider config={config}>
+      <Box width="100%" justifyContent="center" alignItems="center" marginTop={120}>
+        <Login />
+      </Box>
+    </GluestackUIProvider>
   );
 }
