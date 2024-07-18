8d71fdd7e02ae19218185c53eeb6e16e2c5ee5b5
Alex Dana <alex.dana2013@gmail.com>
2024-07-18 23:57:44 +0200

    add game screen

--- app/views/GameScreen.tsx
+++ app/views/GameScreen.tsx
@@ -1,13 +1,20 @@
 import React from "react";
-import { View, StyleSheet, Text, Button } from "react-native";
+import { View, StyleSheet, Image, Text, ImageBackground } from "react-native";
 import { GameTabProps } from "../types";
 import { Colors } from "../constants/Colors";
 import Ionicons from "@expo/vector-icons/Ionicons";
+import { useUserStore } from "../stores";
+import CircularProgress from "react-native-circular-progress-indicator";
 
-const GameScreen = ({ navigation }: GameTabProps) => {
+const GameScreen = ({ navigation, route }: GameTabProps) => {
+  const { userCompletionProgress, userProgressPerGame } = useUserStore();
+  const currentGame = userCompletionProgress?.Results.find(
+    (r) => r.GameID === route.params.gameId,
+  );
+  const userProgression = userProgressPerGame[route.params.gameId];
+  if (!currentGame) return <></>;
+
   React.useEffect(() => {
-    // Use `setOptions` to update the button that we previously specified
-    // Now the button includes an `onPress` handler to update the count
     navigation.setOptions({
       header: () => (
         <View style={{ width: 50, margin: 15, marginTop: 25 }}>
@@ -22,15 +29,68 @@ const GameScreen = ({ navigation }: GameTabProps) => {
     });
   }, [navigation]);
 
-  return <View style={styles.container}></View>;
+  return (
+    <View style={styles.container}>
+      <ImageBackground
+        style={styles.topContainer}
+        imageStyle={{ resizeMode: "cover" }}
+        blurRadius={1}
+        source={{
+          uri: "https://retroachievements.org" + currentGame.ImageIcon,
+        }}
+      >
+        <View style={styles.innerContainer}>
+          <Image
+            source={{
+              uri: "https://retroachievements.org" + currentGame.ImageIcon,
+            }}
+            style={styles.image}
+          />
+          <Text style={styles.textTitle}>{currentGame.Title}</Text>
+          <CircularProgress
+            radius={25}
+            value={
+              (userProgression.NumAchieved /
+                userProgression.NumPossibleAchievements) *
+              100
+            }
+            valueSuffix={"%"}
+            progressValueColor="#ffff"
+            activeStrokeColor={Colors.dark.primary}
+            activeStrokeSecondaryColor={Colors.dark.secondary}
+          />
+        </View>
+      </ImageBackground>
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
+  image: {
+    width: 150,
+    height: 150,
+  },
+  topContainer: {
+    flex: 1,
+    maxHeight: "45%",
+  },
+  innerContainer: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    flexDirection: "column",
+    backgroundColor: "rgba(0,0,0, 0.60)",
+  },
+  textTitle: {
+    marginTop: 15,
+    color: Colors.dark.basicText,
+    fontSize: 25,
+  },
   container: {
     flex: 1,
     flexDirection: "row",
     width: "100%",
-    maxHeight: 50,
+    height: "100%",
   },
 });
 
